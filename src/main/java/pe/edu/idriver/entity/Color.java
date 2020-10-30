29f37ca245da66f95e1154e83b2897c7ed7c478b
Valcz98 <71808218+Valcz98@users.noreply.github.com>
2020-10-29 23:35:59 -0500

    ColorEntity

--- src/main/java/pe/edu/idriver/entity/Color.java
+++ src/main/java/pe/edu/idriver/entity/Color.java
@@ -6,3 +6,38 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+
+@Entity
+@Table(name="Color")
+public class Color {
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue (strategy = GenerationType.IDENTITY)
+	private int idColor;
+	
+	@Column(name="nombreColor", length=60, nullable=false)
+	private String nameColor;
+
+	public int getIdColor() {
+		return idColor;
+	}
+
+	public void setIdColor(int idColor) {
+		this.idColor = idColor;
+	}
+
+	public String getNameColor() {
+		return nameColor;
+	}
+
+	public void setNameColor(String nameColor) {
+		this.nameColor = nameColor;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+}
