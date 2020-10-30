9295b89972d351606737beb758fbda6975483b56
Valcz98 <71808218+Valcz98@users.noreply.github.com>
2020-10-30 00:40:11 -0500

    IDocumentoService

--- src/main/java/pe/edu/idriver/service/IDocumentoService.java
+++ src/main/java/pe/edu/idriver/service/IDocumentoService.java
@@ -1,8 +1,23 @@
-package pe.edu.idriver.entity;
+package pe.edu.idriver.service;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import java.util.List;
+import java.util.Optional;
+
+import pe.edu.idriver.entity.Documento;
+
+public interface IDocumentoService {
+	
+	public boolean insertar(Documento documento);
+	public boolean modificar(Documento documento);
+	public void eliminar(int idDocumento);
+	public Optional<Documento> listarId(int idDocumento);
+	List<Documento> listar();
+	List<Documento> buscarNombre(int idDocumento);
+	List<Documento> buscarUsuario(String nameUsuario);
+	List<Documento> buscarVehiculo(String placaVehiculo);
+	List<Documento> buscarLicencia(int idLicencia);
+	List<Documento> buscarSeguro(int idSeguro);
+	List<Documento> buscarSoat(int idSoat);
+	List<Documento> buscarTarjeta(int idTarjeta);
+	List<Documento> buscarPolarizado(int idPolarizado);
+}
