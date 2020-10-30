673ca1bec85985dc6bea15986e52742cb1e06f6a
Valcz98 <71808218+Valcz98@users.noreply.github.com>
2020-10-30 00:10:30 -0500

    DepartmentEntity

--- src/main/java/pe/edu/idriver/entity/Department.java
+++ src/main/java/pe/edu/idriver/entity/Department.java
@@ -6,3 +6,41 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+
+@Entity
+@Table(name="Departamento")
+public class Department {
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue (strategy = GenerationType.IDENTITY)
+	private int idDepartment;
+	
+	@Column(name="nombreDepartamento", length=60, nullable=false)
+	private String nameDepartment;
+
+	public int getIdDepartment() {
+		return idDepartment;
+	}
+
+	public void setIdDepartment(int idDepartment) {
+		this.idDepartment = idDepartment;
+	}
+
+	public String getNameDepartment() {
+		return nameDepartment;
+	}
+
+	public void setNameDepartment(String nameDepartment) {
+		this.nameDepartment = nameDepartment;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+	
+	
+	
+	
+}
