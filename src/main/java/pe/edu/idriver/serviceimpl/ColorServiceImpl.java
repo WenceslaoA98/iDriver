8f5378942f3d8b7576bf11f4535011ff34f30a45
Valcz98 <71808218+Valcz98@users.noreply.github.com>
2020-10-30 00:44:50 -0500

    ColorServiceImpl

--- src/main/java/pe/edu/idriver/serviceimpl/ColorServiceImpl.java
+++ src/main/java/pe/edu/idriver/serviceimpl/ColorServiceImpl.java
@@ -1,8 +1,71 @@
-package pe.edu.idriver.entity;
+package pe.edu.idriver.serviceimpl;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import pe.edu.idriver.entity.Color;
+import pe.edu.idriver.repository.IColorRepository;
+import pe.edu.idriver.service.IColorService;
+
+@Service
+public class ColorServiceImpl implements IColorService {
+
+	@Autowired
+	private IColorRepository dColor;
+
+	@Override
+	@Transactional
+	public boolean insertar(Color color) {
+		
+		Color objColor = dColor.save(color);
+		if (objColor == null)
+			return false;
+		else return true;
+	}
+
+	@Override
+	@Transactional
+	public boolean modificar(Color color) {
+		
+		boolean flag = false;
+		try {
+			dColor.save(color);
+			flag = true;
+		} catch (Exception e) {
+			System.out.println("Sucedio un roche...");
+		}
+		return flag;
+	}
+
+	@Override
+	@Transactional
+	public void eliminar(int idColor) {
+		
+		dColor.deleteById(idColor);
+	}
+
+	@Override
+	@Transactional(readOnly = true)
+	public Optional<Color> listarId(int idColor) {
+		
+		return dColor.findById(idColor);
+	}
+
+	@Override
+	@Transactional
+	public List<Color> listar() {
+		
+		return dColor.findAll();
+	}
+
+	@Override
+	@Transactional
+	public List<Color> buscarNombre(String nameColor) {
+		
+		return dColor.buscarNombre(nameColor);
+	}
+}
