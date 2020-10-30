62f5e29b57d30c06bf4d847de1a6b8ed3d37b35b
WenceslaoA98 <wenceslaoa_98_@hotmail.com>
2020-10-29 23:02:33 -0500

    first commit

---
+++ src/main/java/pe/edu/idriver/IDriverApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.idriver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IDriverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IDriverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/idriver/IDriverApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.idriver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IDriverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
