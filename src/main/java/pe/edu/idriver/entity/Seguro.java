9d4dc1a3f782861a15437712f66d4a57d40d23e0
WenceslaoA98 <wenceslaoa_98_@hotmail.com>
2020-10-30 00:06:09 -0500

    Mas Entitys

--- src/main/java/pe/edu/idriver/entity/Seguro.java
+++ src/main/java/pe/edu/idriver/entity/Seguro.java
@@ -1,8 +1,93 @@
 package pe.edu.idriver.entity;
 
+import java.util.Date;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+@Entity
+@Table(name="Seguro")
+public class Seguro {
+
+	@Id
+	@Column(name = "idSeguro", nullable = false, length = 11)
+	private int idSeguro;
+	
+	@ManyToOne
+	@JoinColumn(name="idUsuario", nullable = false)
+	private Usuario usuario;
+	
+	@ManyToOne
+	@JoinColumn(name="idVehiculo", nullable = false)
+	private Vehiculo vehiculo;
+	
+	@Column(name="montoSeguro", length=10, nullable=false)
+	private String montoSeguro;
+	
+	@Temporal(TemporalType.DATE)
+	@Column(name="fechaExpedicion")
+	@DateTimeFormat(pattern="yyyy-MM-dd")
+	private Date expedicionSeguro;
+	
+	@Temporal(TemporalType.DATE)
+	@Column(name="fechaExpiracion")
+	@DateTimeFormat(pattern="yyyy-MM-dd")
+	private Date expiracionSeguro;
+
+	public int getIdSeguro() {
+		return idSeguro;
+	}
+
+	public void setIdSeguro(int idSeguro) {
+		this.idSeguro = idSeguro;
+	}
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+
+	public Vehiculo getVehiculo() {
+		return vehiculo;
+	}
+
+	public void setVehiculo(Vehiculo vehiculo) {
+		this.vehiculo = vehiculo;
+	}
+
+	public String getMontoSeguro() {
+		return montoSeguro;
+	}
+
+	public void setMontoSeguro(String montoSeguro) {
+		this.montoSeguro = montoSeguro;
+	}
+
+	public Date getExpedicionSeguro() {
+		return expedicionSeguro;
+	}
+
+	public void setExpedicionSeguro(Date expedicionSeguro) {
+		this.expedicionSeguro = expedicionSeguro;
+	}
+
+	public Date getExpiracionSeguro() {
+		return expiracionSeguro;
+	}
+
+	public void setExpiracionSeguro(Date expiracionSeguro) {
+		this.expiracionSeguro = expiracionSeguro;
+	}
+}
+
--- src/main/java/pe/edu/idriver/entity/Usuario.java
+++ src/main/java/pe/edu/idriver/entity/Usuario.java
@@ -1,8 +1,105 @@
 package pe.edu.idriver.entity;
 
+import java.util.Date;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+@Entity
+@Table(name="Usuario")
+public class Usuario {
+
+	@Id
+	@Column(name = "idUsuario", nullable = false, length = 9)
+	private int idUsuario;
+	
+	@Column(name="nombreUsuario", length=60, nullable=false)
+	private String nameUsuario;
+	
+	@Column(name="apellidoUsuario", length=60, nullable=false)
+	private String lastnameUsuario;
+	
+	@Temporal(TemporalType.DATE)
+	@Column(name="fechaNacMascota")
+	@DateTimeFormat(pattern="yyyy-MM-dd")
+	private Date birthDateUsuario;
+	
+	@Column(name="correoUsuario", length=60, nullable=false)
+	private String emailUsuario;
+	
+	@Column(name="direccionUsuario", length=60, nullable=false)
+	private String addressUsuario;
+	
+	@ManyToOne
+	@JoinColumn(name="idDistrict", nullable = false)
+	private District district;
+
+	public int getIdUsuario() {
+		return idUsuario;
+	}
+
+	public void setIdUsuario(int idUsuario) {
+		this.idUsuario = idUsuario;
+	}
+
+	public String getNameUsuario() {
+		return nameUsuario;
+	}
+
+	public void setNameUsuario(String nameUsuario) {
+		this.nameUsuario = nameUsuario;
+	}
+
+	public String getLastnameUsuario() {
+		return lastnameUsuario;
+	}
+
+	public void setLastnameUsuario(String lastnameUsuario) {
+		this.lastnameUsuario = lastnameUsuario;
+	}
+
+	public Date getBirthDateUsuario() {
+		return birthDateUsuario;
+	}
+
+	public void setBirthDateUsuario(Date birthDateUsuario) {
+		this.birthDateUsuario = birthDateUsuario;
+	}
+
+	public String getEmailUsuario() {
+		return emailUsuario;
+	}
+
+	public void setEmailUsuario(String emailUsuario) {
+		this.emailUsuario = emailUsuario;
+	}
+
+	public String getAddressUsuario() {
+		return addressUsuario;
+	}
+
+	public void setAddressUsuario(String addressUsuario) {
+		this.addressUsuario = addressUsuario;
+	}
+
+	public District getDistrict() {
+		return district;
+	}
+
+	public void setDistrict(District district) {
+		this.district = district;
+	}
+
+	
+	
+}
+
+
