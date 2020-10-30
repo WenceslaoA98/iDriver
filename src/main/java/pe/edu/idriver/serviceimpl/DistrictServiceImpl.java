1ecc00569f6ac8df2fb9eab21c47b9db712d3333
Valcz98 <71808218+Valcz98@users.noreply.github.com>
2020-10-30 00:45:24 -0500

    DistrictServiceImpl

--- src/main/java/pe/edu/idriver/serviceimpl/DistrictServiceImpl.java
+++ src/main/java/pe/edu/idriver/serviceimpl/DistrictServiceImpl.java
@@ -1,8 +1,78 @@
-package pe.edu.idriver.entity;
+package pe.edu.idriver.serviceimpl;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import pe.edu.idriver.entity.District;
+import pe.edu.idriver.repository.IDistrictRepository;
+import pe.edu.idriver.service.IDistrictService;
+
+@Service
+public class DistrictServiceImpl implements IDistrictService {
+
+	@Autowired
+	private IDistrictRepository dDistrict;
+
+	@Override
+	@Transactional
+	public boolean insertar(District district) {
+		
+		District objDistrict = dDistrict.save(district);
+		if (objDistrict == null)
+			return false;
+		else return true;
+	}
+
+	@Override
+	@Transactional
+	public boolean modificar(District district) {
+		
+		boolean flag = false;
+		try {
+			dDistrict.save(district);
+			flag = true;
+		} catch (Exception e) {
+			System.out.println("Sucedio un roche...");
+		}
+		return flag;
+	}
+
+	@Override
+	@Transactional
+	public void eliminar(int idDistrict) {
+		
+		dDistrict.deleteById(idDistrict);
+	}
+
+	@Override
+	@Transactional(readOnly = true)
+	public Optional<District> listarId(int idDistrict) {
+		
+		return dDistrict.findById(idDistrict);
+	}
+
+	@Override
+	@Transactional
+	public List<District> listar() {
+		
+		return dDistrict.findAll();
+	}
+
+	@Override
+	@Transactional
+	public List<District> buscarNombre(String nameDistrict) {
+		
+		return dDistrict.buscarNombre(nameDistrict);
+	}
+	
+	@Override
+	@Transactional
+	public List<District> buscarProvince(String nameProvince) {
+
+		return dDistrict.buscarProvince(nameProvince);
+	}
+}
