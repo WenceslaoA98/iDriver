87ff2fe80c1e6982e47c8f934a44fd6d1a1562f3
Valcz98 <71808218+Valcz98@users.noreply.github.com>
2020-10-30 00:36:30 -0500

    ICarModelService

--- src/main/java/pe/edu/idriver/service/ICarModelService.java
+++ src/main/java/pe/edu/idriver/service/ICarModelService.java
@@ -1,8 +1,17 @@
-package pe.edu.idriver.entity;
+package pe.edu.idriver.service;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import java.util.List;
+import java.util.Optional;
+
+import pe.edu.idriver.entity.CarModel;
+
+public interface ICarModelService {
+	
+	public boolean insertar(CarModel carmodel);
+	public boolean modificar(CarModel carmodel);
+	public void eliminar(int idCarModel);
+	public Optional<CarModel> listarId(int idCarModel);
+	List<CarModel> listar();
+	List<CarModel> buscarNombre(String nameCarModel);
+	List<CarModel> buscarBrand(String nameBrand);
+}
