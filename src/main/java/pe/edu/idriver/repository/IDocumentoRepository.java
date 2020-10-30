e92af113ea79299250e4fa9952225627e78e09e3
Valcz98 <71808218+Valcz98@users.noreply.github.com>
2020-10-30 00:29:52 -0500

    IDocumentoRepository

--- src/main/java/pe/edu/idriver/repository/IDocumentoRepository.java
+++ src/main/java/pe/edu/idriver/repository/IDocumentoRepository.java
@@ -1,8 +1,40 @@
-package pe.edu.idriver.entity;
+package pe.edu.idriver.repository;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import pe.edu.idriver.entity.Documento;
+
+@Repository
+public interface IDocumentoRepository extends JpaRepository<Documento, Integer>{
+
+	@Query("from Documento do where do.idDocumento like %:idDocumento%")
+	List<Documento> buscarNombre(@Param("idDocumento") int idDocumento);
+	
+	@Query("from Documento do where do.usuario.nameUsuario like %:nameUsuario%")
+	List<Documento> buscarUsuario(@Param("nameUsuario") String nameUsuario);
+	
+	@Query("from Documento do where do.vehiculo.placaVehiculo like %:placaVehiculo%")
+	List<Documento> buscarVehiculo(@Param("placaVehiculo") String placaVehiculo);
+	
+	@Query("from Documento do where do.licencia.idLicencia like %:idLicencia%")
+	List<Documento> buscarLicencia(@Param("idLicencia") int idLicencia);
+	
+	@Query("from Documento do where do.seguro.idSeguro like %:idSeguro%")
+	List<Documento> buscarSeguro(@Param("idSeguro") int idSeguro);
+	
+	@Query("from Documento do where do.soat.idSoat like %:idSoat%")
+	List<Documento> buscarSoat(@Param("idSoat") int idSoat);
+	
+	@Query("from Documento do where do.tarjeta.idTarjeta like %:idTarjeta%")
+	List<Documento> buscarTarjeta(@Param("idTarjeta") int idTarjeta);
+	
+	@Query("from Documento do where do.polarizado.idPolarizado like %:idPolarizado%")
+	List<Documento> buscarPolarizado(@Param("idPolarizado") int idPolarizado);
+	
+	
+}
